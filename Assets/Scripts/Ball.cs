9ac1300ca8d33989ea7fce796588a6a47f67b687
XDDale <tz2425@nyu.edu>
2024-02-25 22:12:47 -0500

    1

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Ball : MonoBehaviour
 {
+   
     private void OnCollisionEnter2D(Collision2D collision)
     {
         string tag = collision.gameObject.tag;
@@ -17,8 +19,16 @@ public class Ball : MonoBehaviour
                 GameManager.instance.UpdateScore(10, 1);
                 break;
 
-            case "Point":
+            case "Point1":
                 GameManager.instance.UpdateScore(20, 1);
+                break;
+
+            case "Point2":
+                GameManager.instance.UpdateScore(10, 1);
+                break;
+
+            case "Point3":
+                GameManager.instance.UpdateScore(30, 1);
                 break;
 
             case "Flipper":
